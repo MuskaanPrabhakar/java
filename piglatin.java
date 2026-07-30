f627b97b3ba40ad99a792877f1cac6dee479c457
Muskaan <mskn36.r@gmail.com>
2026-07-30 14:57:51 +0100

    making it faster

--- piglatin.java
+++ piglatin.java
@@ -1,18 +1,16 @@
 public class piglatin {// class started
     public void display(String word){//method started
-        String result="";
         word= word.toUpperCase();
         int first_vowel_at = word.length();
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
             if (ch == 'A' ||ch == 'E' ||ch == 'I' || ch == 'O' || ch == 'U'){
                 first_vowel_at=i;
-                System.out.println("Word in Piglatin: "+ word.substring(first_vowel_at ,word.length()) + result + "AY");
+                System.out.println("Word in Piglatin: "+ word.substring(first_vowel_at ,word.length()) + word.substring(0 ,first_vowel_at-1) + "AY");
                 break;
-            } 
-            else result += ch;
+            }
         }
-        if(first_vowel_at==word.length()) System.out.println("Word in Piglatin: " + result + "AY");
+        if(first_vowel_at==word.length()) System.out.println("Word in Piglatin: " + word + "AY");
     }
     public static void main(String[] args) {
         piglatin obj = new piglatin();
